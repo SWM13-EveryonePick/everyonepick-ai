d06c251553c56fb609cb0a7eaec9ad2e4238f7c6
이주연 <lewndus7749@naver.com>
2022-07-25 18:01:05 +0900

    [PICK-63] UPDATE: 주석 추가 및 불필요한 코드 삭제

--- Face_Recognition/get_image_from_webcam.py
+++ Face_Recognition/get_image_from_webcam.py
@@ -4,6 +4,7 @@ import numpy as np
 import time
 
 mp_face_mesh = mp.solutions.face_mesh
+
 face_mesh = mp_face_mesh.FaceMesh(
             max_num_faces=1,
             refine_landmarks=True,
@@ -13,22 +14,22 @@ face_mesh = mp_face_mesh.FaceMesh(
 mp_drawing = mp.solutions.drawing_utils
 
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
-mp_drawing_styles= mp.solutions.drawing_styles
+mp_drawing_styles = mp.solutions.drawing_styles
 
 
 cap = cv2.VideoCapture(0)
+
 while cap.isOpened():
     success, image = cap.read()
 
     start = time.time()
     if not success:
         print("Ignoring empty camera frame.")
-        # If loading a video, use 'break' instead of 'continue'.
-        continue
+        break
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
+    # To improve performance, optionally mark the image as not writeable to pass by reference.
     image.flags.writeable = False
+    # Flip the image horizontally for a later selfie-view display
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
 
     # Get the result
@@ -45,12 +46,18 @@ while cap.isOpened():
     if results.multi_face_landmarks:
         for face_landmarks in results.multi_face_landmarks:
             for idx, lm in enumerate(face_landmarks.landmark):
+                # idx: 1 -> Tip of the nose
+                # idx: 61 -> Left corner of the mouth
+                # idx: 291 -> Right corner of the mouth
+                # idx: 199 -> Chin
+                # idx: 33 -> Left corner of the left eye
+                # idx: 263 -> Right corner of the right eye
                 if idx == 33 or idx == 263 or idx == 1 or idx == 61 or idx == 291 or idx == 199:
                     if idx == 1:
                         nose_2d = (lm.x * img_w, lm.y * img_h)
-                        nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 3000)
+                        nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 1000)
 
-                    # 정규화 되어 있기 때문에 원래 좌표로 옮기기
+                    # 정규화 되어 있기 때문에 이미지의 높이, 너비 반영하기
                     x, y = int(lm.x * img_w), int(lm.y * img_h)
 
                     # Get the 2D Coordinates
@@ -73,12 +80,14 @@ while cap.isOpened():
 
             # The distortion parameters
             dist_matrix = np.zeros((4, 1), dtype=np.float64)
+            # print(dist_matrix)
 
             # Solve PnP
             success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
 
             # Get rotational matrix
             rmat, jac = cv2.Rodrigues(rot_vec)
+
 
             # Get angles
             angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
@@ -101,8 +110,7 @@ while cap.isOpened():
                 text = "Forward"
 
             # Display the nose direction
-            nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rot_vec, trans_vec, cam_matrix,
-                                                                 dist_matrix)
+            nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rot_vec, trans_vec, cam_matrix, dist_matrix)
 
             p1 = (int(nose_2d[0]), int(nose_2d[1]))
             p2 = (int(nose_2d[0] + y * 10), int(nose_2d[1] - x * 10))
@@ -134,13 +142,7 @@ while cap.isOpened():
                 landmark_drawing_spec=None,
                 connection_drawing_spec=mp_drawing_styles
                 .get_default_face_mesh_contours_style())
-        mp_drawing.draw_landmarks(
-                image=image,
-                landmark_list=face_landmarks,
-                connections=mp_face_mesh.FACEMESH_IRISES,
-                landmark_drawing_spec=None,
-                connection_drawing_spec=mp_drawing_styles
-                .get_default_face_mesh_iris_connections_style())
+
     # Flip the image horizontally for a selfie-view display.
     cv2.imshow('MediaPipe Face Mesh', image)
     if cv2.waitKey(5) & 0xFF == 27:
