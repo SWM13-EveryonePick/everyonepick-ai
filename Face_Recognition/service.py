7b359e3978e409f774e42bc722072f313bffd2b9
이주연 <lewndus7749@naver.com>
2022-10-17 20:05:18 +0900

    [PICK-143] MODIFT: bentoml custom model runner 구조를 위한 코드 수정

--- Face_Recognition/service.py
+++ Face_Recognition/service.py
@@ -5,17 +5,19 @@ from bentoml.io import *
 import cv2
 import numpy as np
 
-face_detect_runner = bentoml.Runner(FaceDetectRunnable, name="face_detect_runner")
-face_recognize_runner = bentoml.Runner(FaceRecognitionRunnable, name="face_recognize_runner")
+
+face_detect_runner = bentoml.Runner(FaceDetectRunnable, name="face_detect", models=[bentoml.onnx.get("face_detection:latest")])
+face_recognize_runner = bentoml.Runner(FaceRecognitionRunnable, name="face_recognize_runner", models=[bentoml.onnx.get("face_recognition:latest")])
 
 svc = bentoml.Service("face_recognition", runners=[face_detect_runner, face_recognize_runner])
-input_spec = Multipart(img=Image(), user_id=Text())
+
+input_spec = Multipart(image=Image(), user_id=Text())
 output_spec = NumpyNdarray()
 
 
 @svc.api(input=input_spec, output=output_spec)
-async def recognize(img, user_id):
-    np_img = np.array(img)
+async def recognize(image, user_id):
+    np_img = np.array(image)
     cv_img = cv2.cvtColor(np_img, cv2.COLOR_RGB2BGR)
     bboxes, kpss = await face_detect_runner.detect.async_run(cv_img)
     embedding = await face_recognize_runner.recognize.async_run(cv_img, kpss[0])
